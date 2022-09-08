d4e7ca7c3ca1fc0283e1df4017180ecb00412fde
sacibaby <111527520+sacibaby@users.noreply.github.com>
2022-09-08 12:35:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.owner = ['+94770378874']
+global.premium = ['+94770378874']
+global.ownernomer = '+94770378874'
+global.ownername = '✌SUNETH MD🍇'
+global.botname = 'Gojo-Satoru privet editor '
+global.footer = '©SUNETH .'
+global.ig = 'httpsසැප කනේ කන සාක්කුවේ '
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'global.packname = 'Suneth 
+global.author = 'suneth '
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
